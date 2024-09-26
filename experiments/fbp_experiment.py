Fixes FBP scaling issue. LIKELY BREAKS FDK code. Adds tomography_utils file. Cleaned fbp_recon code.

experiments/fbp_experiment.py
@@ -34,7 +34,7 @@ sinogram = ct_model_for_generation.forward_project(phantom)
 
 # View sinogram
 title = "Original sinogram \nUse the sliders to change the view or adjust the intensity range."
-mbirjax.slice_viewer(sinogram, slice_axis=0, title=title, slice_label="View")
+# mbirjax.slice_viewer(sinogram, slice_axis=0, title=title, slice_label="View")
 
 # Initialize the model for reconstruction.
 ct_model_for_recon = mbirjax.ParallelBeamModel(sinogram_shape, angles)
@@ -44,8 +44,8 @@ weights = None
 # weights = ct_model_for_recon.gen_weights(sinogram / sinogram.max(), weight_type='transmission_root')
 
 # Set reconstruction parameter values
-# Increase sharpness by 1 or 2 to get clearer edges, possibly with more high-frequency artifacts. ....
-# ... Decrease by 1 or 2 to get softer edges and smoother interiors.
+# Increase sharpness by 1 or 2 to get clearer edges, possibly with more high-frequency ...
+# ... artifacts. Decrease by 1 or 2 to get softer edges and smoother interiors.
 sharpness = 0.0
 ct_model_for_recon.set_params(sharpness=sharpness)
 
@@ -59,19 +59,12 @@ ct_model_for_recon.print_params()
 # Perform VCD reconstruction
 print("Starting recon", end="\n\n")
 time0 = time.time()
-recon = ct_model_for_recon.fbp_recon(sinogram)
+filter = "Ram-Lak" 
+recon = ct_model_for_recon.fbp_recon_vmap(sinogram, filter=filter)
 
 recon.block_until_ready()
 elapsed = time.time() - time0
 print(f"Elapsed time for recon is {elapsed} seconds", end="\n\n")
-
-# TODO: Solve the scaling issues of fbp_recon
-def scale_to_unit_range(array):
-    min_val = np.min(array)
-    max_val = np.max(array)
-    return (array - min_val) / (max_val - min_val)
-
-recon = scale_to_unit_range(recon)
 
 # Compute descriptive statistics about recon result
 max_diff = np.amax(np.abs(phantom - recon))
@@ -85,5 +78,5 @@ print(f"95% of recon pixels are within {pct_95} of phantom")
 mbirjax.get_memory_stats()
 
 # Display results
-title = "Phantom (left) vs filtered back projection (right) \nUse the sliders to change the slice or adjust the intensity range."
+title = f"Phantom (left) vs filtered back projection (right). Filter used: {filter}. \nUse the sliders to change the slice or adjust the intensity range."
 mbirjax.slice_viewer(phantom, recon, title=title)

mbirjax/parallel_beam.py
@@ -3,6 +3,7 @@ import jax.numpy as jnp
 from functools import partial
 from collections import namedtuple
 from mbirjax import TomographyModel, ParameterHandler
+from mbirjax.tomography_utils import generate_filter  # see fbp_recon method
 
 
 class ParallelBeamModel(TomographyModel):
@@ -301,78 +302,67 @@ class ParallelBeamModel(TomographyModel):
 
         return x
     
-    def _create_ramp_filter(self, num_channels):
-        """
-        Create the ramp filter in the time domain using sinc functions.
-
-        Args:
-            num_channels (int): Number of detector channels in the sinogram.
-
-        Returns:
-            ramp_filter (jax array): The computed ramp filter.
-        """
-        n = jnp.arange(-num_channels + 1, num_channels)  # Range for symmetric filter ...
-        # ... ex: num_channels = 3, -> n = [-2, -1, 0, 1, 2]
-        ramp_filter = (1 / 2) * jnp.sinc(n) - (1 / 4) * (jnp.sinc(n / 2)) ** 2
-
-        return ramp_filter
-    
-    def fbp_recon(self, sinogram):
+    def fbp_recon_reshape(self, sinogram, filter="Ram-Lak"):
         """
         Perform filtered back-projection (FBP) reconstruction on the given sinogram.
 
         Args:
             sinogram (jax array): The input sinogram with shape (num_views, num_rows, num_channels).
+            filter (string, optional): Name of the filter to be used. Defaults to "Ram-Lak."
 
         Returns:
             recon (jax array): The reconstructed volume after back-projection.
         """        
         # Generate the filter
         num_views, num_rows, num_channels = sinogram.shape
-        ramp_filter = self._create_ramp_filter(num_channels) 
+        filter = generate_filter(num_channels, filter=filter) 
 
         # Flatten the sinogram (from 3D to 2D)
         sinogram_flattened = sinogram.reshape(-1, sinogram.shape[-1])  # Shape: (num_views * num_rows, num_channels)
 
-        # Define convolution func that will be applied to each channel
-        def convolve_row(channel):
-            return jnp.convolve(channel, ramp_filter, mode="valid")
+        # Define convolution for a single row (across its channels)
+        def convolve_row(row):
+            return jnp.convolve(row, filter, mode="valid")
 
-        # Apply convolution to all channels, iterating over (views x rows)
+        # Apply convolution across the channels, iterating over (views x rows)
         filtered_sinogram_flattened = jax.vmap(convolve_row)(sinogram_flattened)
 
         # Reshape the filtered sinogram back to (views, rows, channels)
         filtered_sinogram = filtered_sinogram_flattened.reshape(num_views, num_rows, -1)
 
         recon = self.back_project(filtered_sinogram) 
+        recon *= jnp.pi / num_views  # scaling term
 
         return recon
     
-    def fbp_recon_2(self, sinogram):
-        # Define the filter
-        def generate_ramp_fiter(num_channels):
-            n = jnp.arange(-num_channels + 1, num_channels)  # Range for symmetric filter ...
-            # ... ex: num_channels = 3, -> n = [-2, -1, 0, 1, 2]
-            ramp_filter = (1 / 2) * jnp.sinc(n) - (1 / 4) * (jnp.sinc(n / 2)) ** 2
+    def fbp_recon_vmap(self, sinogram, filter="Ram-Lak"):
+        """
+        Perform filtered back-projection (FBP) reconstruction on the given sinogram.
 
-            return ramp_filter
-        
+        Args:
+            sinogram (jax array): The input sinogram with shape (num_views, num_rows, num_channels).
+            filter (string, optional): Name of the filter to be used. Defaults to "Ram-Lak."
+
+        Returns:
+            recon (jax array): The reconstructed volume after back-projection.
+        """      
         # Generate the filter
-        num_channels = sinogram.shape[2]
-        kernel = generate_ramp_fiter(num_channels) 
+        num_views, _, num_channels = sinogram.shape
+        filter = generate_filter(num_channels, filter=filter) 
 
-        # Define convolution across a single channel
-        def convolve_channel(channel):
-            return jnp.convolve(channel, kernel, mode="valid")
+        # Define convolution for a single row (across its channels)
+        def convolve_row(row):
+            return jnp.convolve(row, filter, mode="valid")
 
-        # Define a function to apply convolution to each row (which consists of channels)
-        def apply_convolution_to_row(row):
-            return jax.vmap(convolve_channel)(row)
+        # Apply above convolve func across each row of a view
+        def apply_convolution_to_view(view):
+            return jax.vmap(convolve_row)(view) 
 
-        # Apply convolution to all views and rows in the sinogram using vmap
-        filtered_sinogram = jax.vmap(apply_convolution_to_row)(sinogram)
+        # Apply convolution across the channels of the sinogram per each fixed view & row
+        filtered_sinogram = jax.vmap(apply_convolution_to_view)(sinogram)
 
         recon = self.back_project(filtered_sinogram)
+        recon *= jnp.pi / num_views  # scaling term
 
         return recon
     