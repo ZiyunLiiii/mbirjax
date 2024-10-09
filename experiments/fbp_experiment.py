Merges fbp & fdk unit test into single file. Everything working except for FDK scaling bug.

experiments/fbp_experiment.py
@@ -1,66 +1,51 @@
 """
-**MBIRJAX: Filtered back projection FBP basic demo**
+**MBIRJAX: Filtered back projection (FBP) basic demo**
 
 This is a modified version of the demo_1_shepp_logan.py file that is more streamlined and uses filtered back projection insetad of the standard recon.
 
 """
-
 import numpy as np
 import time
 import jax.numpy as jnp
 import mbirjax
 
+
 # Set geometry parameters
-geometry_type = "parallel"  # FBP is necessarily parallel, but I still define it as a "parameter"
-num_views = 256
+num_views = 64
 num_det_rows = 128
 num_det_channels = 128
-detector_cone_angle = 0
-start_angle = -(np.pi + detector_cone_angle) * (1/2)
-end_angle = (np.pi + detector_cone_angle) * (1/2)
+start_angle = -(jnp.pi) * (1/2)
+end_angle = (jnp.pi) * (1/2)
+angles = jnp.linspace(start_angle, end_angle, num_views, endpoint=False)
 
 # Initialize sinogram
 sinogram_shape = (num_views, num_det_rows, num_det_channels)
-angles = jnp.linspace(start_angle, end_angle, num_views, endpoint=False)
-ct_model_for_generation = mbirjax.ParallelBeamModel(sinogram_shape, angles)
+parallel_model = mbirjax.ParallelBeamModel(sinogram_shape, angles)
 
 # Generate 3D Shepp Logan phantom
 print("Creating phantom", end="\n\n")
-phantom = ct_model_for_generation.gen_modified_3d_sl_phantom()
+phantom = parallel_model.gen_modified_3d_sl_phantom()
 
 # Generate sinogram from phantom
 print("Creating sinogram", end="\n\n")
-sinogram = ct_model_for_generation.forward_project(phantom)
+sinogram = parallel_model.forward_project(phantom)
 
 # View sinogram
 title = "Original sinogram \nUse the sliders to change the view or adjust the intensity range."
 mbirjax.slice_viewer(sinogram, slice_axis=0, title=title, slice_label="View")
 
-# Initialize the model for reconstruction.
-ct_model_for_recon = mbirjax.ParallelBeamModel(sinogram_shape, angles)
-
-# Generate weights array - for an initial reconstruction, use weights = None, then modify if needed.
-weights = None
-# weights = ct_model_for_recon.gen_weights(sinogram / sinogram.max(), weight_type='transmission_root')
-
-# Set reconstruction parameter values
-# Increase sharpness by 1 or 2 to get clearer edges, possibly with more high-frequency ...
-# ... artifacts. Decrease by 1 or 2 to get softer edges and smoother interiors.
-sharpness = 0.0
-ct_model_for_recon.set_params(sharpness=sharpness)
-
 # Print out model parameters
-ct_model_for_recon.print_params()
+# parallel_model.print_params()
 
 ################################################################################
 # Reconstruction starts here
 ################################################################################
 
-# Perform VCD reconstruction
+# Perform FBP reconstruction
 print("Starting recon", end="\n\n")
 time0 = time.time()
-filter = "Ram-Lak" 
-recon = ct_model_for_recon.fbp_recon_reshape_jit(sinogram, filter=filter)
+filter = "ramp" 
+recon = parallel_model.fbp_recon(sinogram, filter=filter)
 
 recon.block_until_ready()
 elapsed = time.time() - time0

experiments/fdk_experiment.py
@@ -1,87 +1,62 @@
-# -*- coding: utf-8 -*-
 """
-**MBIRJAX: Feldkamp, Davis, and Kress (FDK) cone-beam reconstruction algorithm basic demo**
+**MBIRJAX: Cone-beam reconstruction with FDK method basic demo**
 
-This is a modified version of the demo_1_shepp_logan.py file that its more streamlined and uses FDK reconstruction insetad of the standard recon.
+This is a modified version of the demo_1_shepp_logan.py file that is more streamlined and uses filtered back projection insetad of the standard recon.
 
 """
-
-# Commented out IPython magic to ensure Python compatibility.
-# %pip install mbirjax
-
 import numpy as np
 import time
 import jax.numpy as jnp
 import mbirjax
 
-# Set geometry parameters
-geometry_type = "cone"  # FDK is necessarily cone, but I still define it as a "parameter"
-num_views = 256
-num_det_rows = 40
-num_det_channels = 128
 
-# For cone beam geometry, we need to describe the distances source to detector and source to rotation axis.
-# np.Inf is an allowable value, in which case this is essentially parallel beam
+# Set geometry parameters
+num_views = 64
+num_det_rows = 128
+num_det_channels = 128
 source_detector_dist = 4 * num_det_channels
 source_iso_dist = source_detector_dist / 2
 
 detector_cone_angle = 2 * np.arctan2(num_det_channels / 2, source_detector_dist)
-start_angle = -(np.pi + detector_cone_angle) * (1/2)
-end_angle = (np.pi + detector_cone_angle) * (1/2)
+start_angle = -(jnp.pi + detector_cone_angle) * (1/2)
+end_angle = (jnp.pi + detector_cone_angle) * (1/2)
+angles = jnp.linspace(start_angle, end_angle, num_views, endpoint=False)
 
 # Initialize sinogram
 sinogram_shape = (num_views, num_det_rows, num_det_channels)
-angles = jnp.linspace(start_angle, end_angle, num_views, endpoint=False)
-ct_model_for_generation = mbirjax.ConeBeamModel(sinogram_shape, angles, source_detector_dist=source_detector_dist, source_iso_dist=source_iso_dist)
+cone_model = mbirjax.ConeBeamModel(sinogram_shape, 
+                                   angles, 
+                                   source_detector_dist=source_detector_dist, 
+                                   source_iso_dist=source_iso_dist)
 
 # Generate 3D Shepp Logan phantom
 print("Creating phantom", end="\n\n")
-phantom = ct_model_for_generation.gen_modified_3d_sl_phantom()
+phantom = cone_model.gen_modified_3d_sl_phantom()
 
 # Generate sinogram from phantom
 print("Creating sinogram", end="\n\n")
-sinogram = ct_model_for_generation.forward_project(phantom)
+sinogram = cone_model.forward_project(phantom)
 
 # View sinogram
 title = "Original sinogram \nUse the sliders to change the view or adjust the intensity range."
 mbirjax.slice_viewer(sinogram, slice_axis=0, title=title, slice_label="View")
 
-# Initialize the model for reconstruction.
-ct_model_for_recon = mbirjax.ConeBeamModel(sinogram_shape, angles, source_detector_dist=source_detector_dist, source_iso_dist=source_iso_dist)
-
-# Generate weights array - for an initial reconstruction, use weights = None, then modify if needed.
-weights = None
-# weights = ct_model_for_recon.gen_weights(sinogram / sinogram.max(), weight_type='transmission_root')
-
-# Set reconstruction parameter values
-# Increase sharpness by 1 or 2 to get clearer edges, possibly with more high-frequency artifacts. ....
-# ... Decrease by 1 or 2 to get softer edges and smoother interiors.
-sharpness = 0.0
-ct_model_for_recon.set_params(sharpness=sharpness)
-
 # Print out model parameters
-ct_model_for_recon.print_params()
+# cone_model.print_params()
 
 ################################################################################
 # Reconstruction starts here
 ################################################################################
 
-# Perform VCD reconstruction
+# Perform FDK recon
 print("Starting recon")
 time0 = time.time()
-recon = ct_model_for_recon.fdk_recon(sinogram)
+filter = "ramp" 
+recon = cone_model.fdk_recon(sinogram, filter=filter)
 
 recon.block_until_ready()
 elapsed = time.time() - time0
 print(f"Elapsed time for recon is {elapsed} seconds", end="\n\n")
-
-# TODO: Solve the scaling issues of fbp_recon
-def scale_to_unit_range(array):
-    min_val = np.min(array)
-    max_val = np.max(array)
-    return (array - min_val) / (max_val - min_val)
-
-recon = scale_to_unit_range(recon)
 
 # Compute descriptive statistics about recon result
 max_diff = np.amax(np.abs(phantom - recon))
@@ -95,5 +70,5 @@ print(f"95% of recon pixels are within {pct_95} of phantom")
 mbirjax.get_memory_stats()
 
 # Display results
-title = 'Phantom (left) vs filtered back projection (right) \nUse the sliders to change the slice or adjust the intensity range.'
+title = f"Phantom (left) vs filtered back projection (FDK, right). Filter used: {filter}. \nUse the sliders to change the slice or adjust the intensity range."
 mbirjax.slice_viewer(phantom, recon, title=title)

mbirjax/cone_beam.py
@@ -4,6 +4,7 @@ from functools import partial
 from collections import namedtuple
 import warnings
 import mbirjax
+from mbirjax.tomography_utils import generate_filter  # see fdk_recon method
 
 
 class ConeBeamModel(mbirjax.TomographyModel):
@@ -740,38 +741,53 @@ class ConeBeamModel(mbirjax.TomographyModel):
         # Convert from xyz to coordinates on detector
         pixel_mag = 1 / (1 / gp.magnification - y / gp.source_detector_dist)
         return y, pixel_mag
+    
+    def fdk_recon(self, sinogram, filter="ramp"):
+        # TODO write docstring, mention this only coveres planar detector in doc string.
 
-    def _create_ramp_filter(self, num_channels):
-        """
-        Create the ramp filter in the time domain using sinc functions.
+        num_views, num_rows, num_channels = sinogram.shape
+        filter = generate_filter(num_channels, filter=filter)
+        source_detector_dist, source_iso_dist = self.get_params(['source_detector_dist', 'source_iso_dist'])
 
-        Args:
-            num_channels (int): Number of detector channels in the sinogram.
+        # Define the s and v coordinates (in pixel units)
+        s = (jnp.arange(num_channels) - (num_channels // 2))
+        v = (jnp.arange(num_rows - 1, -1, -1) - (num_rows // 2))  # Reversed to start from the top
 
-        Returns:
-            ramp_filter (jax array): The computed ramp filter.
-        """
-        n = jnp.arange(-num_channels + 1, num_channels)  # Range for symmetric filter ...
-        # ... ex: num_channels = 3, -> n = [-2, -1, 0, 1, 2]
-        ramp_filter = (1 / 2) * jnp.sinc(n) - (1 / 4) * (jnp.sinc(n / 2)) ** 2
+        # Create the meshgrid
+        v_grid, s_grid = jnp.meshgrid(v, s, indexing='ij')
 
-        return ramp_filter
+        # Combine the s and v grids into a single matrix of coordinate pairs
+        coordinates = jnp.stack((s_grid, v_grid), axis=-1)
 
-    def fdk_recon(self, sinogram):
-        """
-        Apply the ramp filter with geometric weighting in the time domain to the sinogram.
+        pre_weight = source_iso_dist / jnp.sqrt(source_iso_dist**2 + jnp.sum(coordinates**2, axis=-1))
 
-        Args:
-            sinogram (jax array): 3D sinogram data with shape (num_views, num_rows, num_channels).
+        # Apply the pre-weighting factor to the sinogram
+        weighted_sinogram = sinogram * pre_weight
 
-        Returns:
-            filtered_sinogram (jax array): Sinogram after applying ramp filter and geometric weighting.
-        """
+        # Define convolution for a single row (across its channels)
+        def convolve_row(row):
+            return jnp.convolve(row, filter, mode="valid")
+
+        # Apply above convolve func across each row of a view
+        def apply_convolution_to_view(view):
+            return jax.vmap(convolve_row)(view)
+
+        # Apply convolution across the channels of the weighted sinogram per each fixed view & row
+        filtered_sinogram = jax.vmap(apply_convolution_to_view)(weighted_sinogram)
+
+        recon = self.back_project(filtered_sinogram)
+        recon *= (jnp.pi / num_views) * (source_detector_dist / source_iso_dist)
+        # recon *= (1 / (2 * num_views * source_iso_dist ** 2))
+        recon *= 2 * jnp.pi / num_views
+
+        return recon
+
+    def fdk_recon_old(self, sinogram):
         num_views, num_rows, num_channels = sinogram.shape
         source_detector_dist, source_iso_dist = self.get_params(['source_detector_dist', 'source_iso_dist'])
 
         # Create the ramp filter in time domain (sinc functions)
-        ramp_filter = self._create_ramp_filter(num_channels)
+        ramp_filter = generate_filter(num_channels, filter="Ram-Lak")
 
         # Flatten the sinogram (from 3D to 2D)
         sinogram_flattened = sinogram.reshape(-1, sinogram.shape[-1])  # Shape: (num_views * num_rows, num_channels)
@@ -798,5 +814,6 @@ class ConeBeamModel(mbirjax.TomographyModel):
         # Apply the weighting to the filtered sinogram, back project
         filtered_sinogram *= weighting[None, :, :]
         recon = self.back_project(filtered_sinogram)
+        # recon *= jnp.pi / num_views  # scaling term
 
         return recon

mbirjax/tomography_utils.py
@@ -1,32 +1,34 @@
 import jax.numpy as jnp
 
-def generate_filter(num_channels, filter="Ram-Lak"):
+
+def generate_filter(num_channels, filter="ramp"):
     """
     Creates the specified filter in the time domain of size (2*num_channels - 1).
 
-    Currently supported filters include: \"Ram-Lak\", \"Shepp-Logan.\"
+    Currently supported filters include: \"ramp\", \"shepp-Logan.\"
 
     Args:
         num_channels (int): Number of detector channels in the sinogram.
-        filter (string, optional): Name of the filter to be generated. Defaults to "Ram-Lak."
+        filter (string, optional): Name of the filter to be generated. Defaults to "ramp."
 
     Returns:
         filter (jnp): The computed filter (filter.size = 2*num_channels + 1).
     """
+    n = jnp.arange(-num_channels + 1, num_channels)  # ex: num_channels = 3, -> n = [-2, -1, 0, 1, 2]
+
+    if filter == "ramp":
+        filter = (1 / 2) * jnp.sinc(n) - (1 / 4) * (jnp.sinc(n / 2)) ** 2
+
+    if filter == "shepp-logan":
+        filter = (-2) / ((jnp.pi ** 2) * (4 * (n)**2 - 1))
+
     # If you want to add a new filter, place it's name into supported_filters, and ...
     # ... create a new if statement with the filter math.
-    supported_filters = ["Ram-Lak", "Shepp-Logan"]
-    n = jnp.arange(-num_channels + 1, num_channels)  # ex: num_channels = 3, -> n = [-2, -1, 0, 1, 2]
+    supported_filters = ["ramp", "shepp-logan"]
     # Raise error if inputed filter is not supported.
     if filter not in supported_filters:
         raise ValueError(f"Unsupported filter. Supported filters are: {', '.join(supported_filters)}.")
     
-    if filter == "Ram-Lak":
-        filter = (1 / 2) * jnp.sinc(n) - (1 / 4) * (jnp.sinc(n / 2)) ** 2
-
-    if filter == "Shepp-Logan":
-        filter = (-2) / ((jnp.pi ** 2) * (4 * (n)**2 - 1))
-
     return filter
 
 